deaf792875e69155891e77a145392f3a46145c34
Harsh Patel <harshaudi27@gmail.com>
2019-06-01 16:28:19 +0530

    Modified categories section

--- app/src/main/java/com/beingdev/magicprint/ShopByCategoryActivity.java
+++ app/src/main/java/com/beingdev/magicprint/ShopByCategoryActivity.java
@@ -25,98 +25,9 @@ import io.appbase.client.AppbaseClient;
 
 public class ShopByCategoryActivity extends AppCompatActivity {
 
-    ListView categoryList;
-    ProgressBar progressBar;
-    TextView fetchingText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_by_category);
-
-        categoryList = (ListView) findViewById(R.id.categoryList);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        fetchingText = (TextView) findViewById(R.id.fetchingText);
-
-        progressBar.setVisibility(View.VISIBLE);
-        fetchingText.setVisibility(View.VISIBLE);
-        LoadCategory loadCategory = new LoadCategory();
-        loadCategory.execute();
-    }
-
-    private class LoadCategory extends AsyncTask<Void, Void, Void> {
-
-        ArrayList<String> arrayList, arrayList2;
-        ArrayList<CategoryItemModel> categories;
-        private CategoryAdapter categoryAdapter;
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            AppbaseClient client = new AppbaseClient("https://scalr.api.appbase.io","shopify-flipkart-test", "xJC6pHyMz", "54fabdda-4f7d-43c9-9960-66ff45d8d4cf");
-
-            arrayList = new ArrayList<>();
-            arrayList2 = new ArrayList<>();
-            categories = new ArrayList<>();
-
-            for(int k = 65; k < 91; k++) {
-
-                arrayList.clear();
-
-                String query =  "{ \"match_phrase_prefix\": { \"tags\": { \"query\": \"" + k + "\", \"analyzer\": \"standard\", \"max_expansions\": 30 } } }";
-
-                try {
-                    String result = client.prepareSearch("products", query)
-                            .execute()
-                            .body()
-                            .string();
-
-                    JSONObject resultJSON = new JSONObject(result);
-                    JSONObject hits = resultJSON.getJSONObject("hits");
-                    JSONArray finalHits = hits.getJSONArray("hits");
-
-                    for(int i = 0; i < finalHits.length(); i++) {
-
-                        JSONObject obj = finalHits.getJSONObject(i);
-                        JSONObject source = obj.getJSONObject("_source");
-                        JSONArray entry = source.getJSONArray("tags");
-
-                        for(int j = 0; j < entry.length(); j++) {
-
-                            String category = entry.getString(j);
-                            arrayList.add(category);
-                        }
-                    }
-
-                    for(int i = 0; i < arrayList.size(); i++) {
-                        if(!arrayList2.contains(arrayList.get(i))) {
-                            arrayList2.add(arrayList.get(i));
-                        }
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                //Log.d("RESULT", arrayList2.toString());
-            }
-
-            for(int i = 0; i < arrayList2.size(); i++) {
-                categories.add(new CategoryItemModel(arrayList2.get(i)));
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-
-            progressBar.setVisibility(View.GONE);
-            fetchingText.setVisibility(View.GONE);
-            categoryAdapter = new CategoryAdapter(categories, getApplicationContext());
-            categoryList.setAdapter(categoryAdapter);
-        }
     }
 }
